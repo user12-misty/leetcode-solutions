517456078e04728335574c5b5f813e9a3b3dd85d
Ridhima Singh <ridhimasingh1211@gmail.com>
2026-08-26 09:46:20 +0530

    Add files via upload

---
+++ Leetcode - practice solutions/leetcode/ARRAY/setMatrixZeros.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+         int m = matrix.size(), n = matrix[0].size(); 
+         vector<bool>row(m,false);
+         vector<bool>col(n,false);
+        for(int i=0; i<m; i++){ 
+            for(int j=0; j<n; j++){ 
+if(matrix[i][j]==0){
+    row[i]=true;
+    col[j]=true;
+
+}     
+            } 
+        } 
+       for(int i=0;i<row.size();i++){
+if(row[i]==true){
+    for(int  j=0;j<n;j++){
+        matrix[i][j]=0;
+    }
+}
+       }  
+       for(int j=0;j<col.size();j++){
+if(col[j]==true){
+    for(int  i=0;i<m;i++){
+        matrix[i][j]=0;
+    }
+}
+       }  
+         } 
+};

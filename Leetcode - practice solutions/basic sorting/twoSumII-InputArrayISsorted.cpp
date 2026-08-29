d65fe4ece3e91a7f19e4c8828627589d7eda4740
Ridhima Singh <ridhimasingh1211@gmail.com>
2026-08-29 12:04:18 +0530

    Add files via upload

---
+++ Leetcode - practice solutions/basic sorting/twoSumII-InputArrayISsorted.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans(2);
+    int n=nums.size();
+    int i=0, j=n-1;
+    while(i<j){
+        if(nums[i]+nums[j]>target) j--;
+        else if(nums[i]+nums[j]<target) i++;
+        else{
+          ans[0]=i+1;
+          ans[1]=j+1;
+          break;
+        }
+    }
+    return ans;
+}
+};

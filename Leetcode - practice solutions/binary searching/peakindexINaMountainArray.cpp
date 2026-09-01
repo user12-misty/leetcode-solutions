7fae48267e9ac7c28525c19ffa6ae0cfe702a152
Ridhima Singh <ridhimasingh1211@gmail.com>
2026-09-01 22:01:22 +0530

    Add files via upload

---
+++ Leetcode - practice solutions/binary searching/peakindexINaMountainArray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+                int n= arr.size();
+        int lo=1, hi=n-2;
+        while(lo<= hi){
+            int mid = lo+(hi-lo)/2;
+            if(arr[mid-1] < arr[mid] && arr[mid]< arr[mid+1] ) lo=mid+1;
+            else if(arr[mid-1] >arr[mid] && arr[mid]>arr[mid+1] ) hi=mid-1;
+            else return mid;
+        }
+        return 1223;
+    }
+};
